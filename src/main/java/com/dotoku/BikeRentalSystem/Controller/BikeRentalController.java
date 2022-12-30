d7bef0f3b3252ee958f1ffa5a593baea969ff74a
sandeshghimire2020 <sandesh.ghimire2020@gmail.com>
2022-12-29 20:01:51 -0600

    adding cross origin
    cross origin for front end

--- src/main/java/com/dotoku/BikeRentalSystem/Controller/BikeRentalController.java
+++ src/main/java/com/dotoku/BikeRentalSystem/Controller/BikeRentalController.java
@@ -7,13 +7,11 @@ import com.dotoku.BikeRentalSystem.Models.RentalDetails;
 import com.dotoku.BikeRentalSystem.Services.BikeRentalServices;
 import org.apache.catalina.User;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 @RequestMapping(path="api/v1/")
 public class BikeRentalController {
